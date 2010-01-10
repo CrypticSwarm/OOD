94662ee85dcbd7badb4a306a8a1fa7a021a0aec3
CrypticSwarm <stardistroyer85+git@gmail.com>
2010-01-09 19:39:30 -0600

    Changing outputers to export a 'output' function that takes an output stream function, and an object to output.

--- textDisplay.js
+++ textDisplay.js
@@ -1,19 +1,21 @@
-var sys = require('sys');
-var textDisplay = function(obj, indent){
-  indent = indent || '';
-  var type;
-  for(var p in obj){
-    type = typeof(obj[p]);
-    if(textDisplay[type]) {
-      sys.puts(indent + p + ':');
-      textDisplay[type](obj[p], indent + '   ');
-    }
-    else sys.puts(indent + p + ': ' + obj[p]);
-  }
-};
+exports.output = function(out, obj){
 
-textDisplay.object = function(obj, indent) {
-	textDisplay(obj, indent);
-};
+	var textDisplay = function(obj, indent){
+		indent = indent || '';
+		var type;
+		for(var p in obj){
+			type = typeof(obj[p]);
+			if(textDisplay[type]) {
+				out(indent + p + ':');
+				textDisplay[type](obj[p], indent + '   ');
+			}
+			else out(indent + p + ': ' + obj[p]);
+		}
+	};
 
-exports.textDisplay = textDisplay;
+	textDisplay.object = function(obj, indent) {
+		textDisplay(obj, indent);
+	};
+	out(obj.name + ' Documentation');
+	textDisplay(obj.doc);
+};
