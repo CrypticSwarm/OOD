0a764da81fbd3ad9904ee7e6162358d9b165d87d
Olmo Maldonado <olmo.maldonado@gmail.com>
2010-01-16 13:53:35 -0600

    Cleaned up Class example and added argument emulation for the _test.html and usage in the CLI.

--- utils/run.js
+++ utils/run.js
@@ -1,4 +1,7 @@
 var docgen = require('./DocumentGen').generateDocs;
 var puts = require('sys').puts;
 
-docgen(puts, '../examples/Request', '../generators/markdown');
+var example = process.ARGV[1] || 'Request';
+var generator = process.ARGV[2] || 'markdown';
+
+docgen(puts, '../examples/' + example, '../generators/' + generator);
