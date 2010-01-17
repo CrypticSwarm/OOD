05a124eb64892681ef964f8c5edd0c2bb87e5a7b
Olmo Maldonado <olmo.maldonado@gmail.com>
2010-01-17 14:32:58 -0600

    Necessary (temporary?) changes for the json output.

--- examples/Class.js
+++ examples/Class.js
@@ -1,21 +1,21 @@
 doc('Class', 'The base Class of the MooTools framework.', function(){
 
-	arg('params', [Function, Object], 'The function constructor or an object with properties that apply to the class.', function(){
-		key('Extends', Class, 'The parent class for this subclass.');
-		key('Implements', [Array, Object, Class], 'Implements is similar to Extends, except that it adopts properties from one or more other classes without inheritance. Useful when implementing a default set of properties in multiple Classes. The Implements property should come after Extends but before all other properties.');
-		key('protected ...', Function, 'Any property that starts with protected keyword will prevent access from outside of the class or subclasses.', function(){
+	arg('params', ['Function', 'Object'], 'The function constructor or an object with properties that apply to the class.', function(){
+		key('Extends', 'Class', 'The parent class for this subclass.');
+		key('Implements', ['Array', 'Object', 'Class'], 'Implements is similar to Extends, except that it adopts properties from one or more other classes without inheritance. Useful when implementing a default set of properties in multiple Classes. The Implements property should come after Extends but before all other properties.');
+		key('protected ...', 'Function', 'Any property that starts with protected keyword will prevent access from outside of the class or subclasses.', function(){
 			example('...', function(){});
 		});
-		key('linked ...', Function, 'Any property that starts with linked keyword will be copied into the property with no other changes.', function(){
+		key('linked ...', 'Function', 'Any property that starts with linked keyword will be copied into the property with no other changes.', function(){
 			example('...', function(){});
 		});
 	});
 
-	returns(Class, 'The created class.');
+	returns('Class', 'The created class.');
 
 	doc('implement', 'Implements the passed in properties into the base Class prototypes, altering the base Class.', function(){
 		note('The same as creating a new Class with the Implements property, but handy when you need to modify existing classes.');
-		arg('properties', Object, 'The properties to add to the base Class.');
+		arg('properties', 'Object', 'The properties to add to the base Class.');
 		example('Include method into the class.', function(){
 			var Animal = new Class({
 				initialize: function(age){
